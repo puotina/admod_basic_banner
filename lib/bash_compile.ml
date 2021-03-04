001790531857a60ab401c935c07d21c3959419fd
puotina <105338854+puotina@users.noreply.github.com>
2021-03-04 05:54:35 +0000

    init script

--- lib/bash_compile.ml
+++ lib/bash_compile.ml
@@ -8,3 +8,59 @@ let is_arith (expr: BAST.expression) :bool =
   match expr with
   | BAST.String _
   | BAST.List _
+  | BAST.StrCompare _
+  | BAST.Concat _
+  | BAST.Call _ ->
+    false
+  | BAST.Bool _
+  | BAST.Int _
+  | BAST.Float _
+  | BAST.Leftvalue _
+  | BAST.ArithUnary _
+  | BAST.ArithBinary _ ->
+    true
+
+let is_leftvalue (expr : BAST.expression) : bool =
+  match expr with
+  | BAST.Leftvalue _ -> true
+  | _ -> false
+
+let rec compile_expr_to_arith
+    (expr: BAST.expression)
+    ~(symtable: Symbol_table.t)
+    ~(scope: Symbol_table.Scope.t)
+  :arithmetic =
+  let compile_expr_to_arith = compile_expr_to_arith ~symtable ~scope in
+  match expr with
+  | BAST.Bool false -> Int 0
+  | BAST.Bool true -> Int 1
+  | BAST.Int number -> Int number
+  | BAST.Float number -> Float number
+  | BAST.Leftvalue lvalue ->
+    Leftvalue (compile_leftvalue lvalue ~symtable ~scope)
+  | BAST.ArithUnary (operator, expr) ->
+    ArithUnary (operator, compile_expr_to_arith expr)
+  | BAST.ArithBinary (operator, left, right) ->
+    ArithBinary (operator,
+                 compile_expr_to_arith left,
+                 compile_expr_to_arith right)
+  | BAST.String _
+  | BAST.List _
+  | BAST.StrCompare _
+  | BAST.Concat _
+  | BAST.Call _ ->
+    assert false
+
+and compile_expr
+    (expr: BAST.expression)
+    ~(symtable: Symbol_table.t)
+    ~(scope: Symbol_table.Scope.t)
+  : expression =
+  if is_arith expr && not (is_leftvalue expr) then
+    Result (compile_expr_to_arith expr ~symtable ~scope)
+  else
+    let compile_expr = compile_expr ~symtable ~scope in
+    match expr with
+    | BAST.Bool false -> String "false"
+    | BAST.Bool true -> String "true"
+    | BAST.Int number -> String (string_of_int number)
