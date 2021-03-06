663984a4c5c515f59fc016c72a55e158db65f12b
puotina <105338854+puotina@users.noreply.github.com>
2021-03-06 20:46:59 +0000

    fix start

--- lib/batsh_format.ml
+++ lib/batsh_format.ml
@@ -4,4 +4,6 @@ open Batsh_ast
 let rec print_lvalue (buf : Buffer.t) (lvalue: leftvalue) =
   match lvalue with
   | Identifier ident ->
-    Buffer.add_string b
+    Buffer.add_string buf ident
+  | ListAccess (lvalue, expr) ->
+    bprintf buf
