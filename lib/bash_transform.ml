79adb026ae5269dc48d449ae3509b7af57fa723b
puotina <105338854+puotina@users.noreply.github.com>
2021-03-06 14:07:47 +0000

    added counter

--- lib/bash_transform.ml
+++ lib/bash_transform.ml
@@ -47,3 +47,54 @@ let rec split_expression
     let assignments, (left, right) = split_binary (left, right)
         ~split_string:true
     in
+    assignments, ArithBinary (operator, left, right)
+  | String str ->
+    split_when ~cond:split_string (Dlist.empty ()) (String str)
+  | Concat (left, right) ->
+    let assignments, (left, right) = split_binary (left, right) in
+    split_when ~cond:split_string assignments (Concat (left, right))
+  | StrCompare (operator, left, right) ->
+    let assignments, (left, right) = split_binary (left, right) in
+    split_when ~cond:split_strcmp assignments (StrCompare (operator, left, right))
+  | Call (ident, exprs) ->
+    let assignments, exprs = split_expressions exprs ~symtable ~scope in
+    split_when ~cond:split_string assignments (Call (ident, exprs))
+  | List exprs ->
+    let assignments, exprs = split_expressions exprs ~symtable ~scope in
+    split_when ~cond:split_list assignments (List exprs)
+
+and split_expressions
+    (exprs : expressions)
+    ~(symtable : Symbol_table.t)
+    ~(scope : Symbol_table.Scope.t)
+  : (statement Dlist.t * expressions) =
+  let assignments, exprs = List.fold exprs ~init: (Dlist.empty (), [])
+      ~f: (fun (assignments_acc, exprs_acc) expr ->
+          let assignments, expr = split_expression expr ~symtable ~scope in
+          (Dlist.append assignments assignments_acc, expr :: exprs_acc)
+        )
+  in
+  assignments, List.rev exprs
+
+let rec split_statement
+    (stmt : statement)
+    ~(symtable : Symbol_table.t)
+    ~(scope : Symbol_table.Scope.t)
+  : statement =
+  let prepend_assignments (assignments : statement Dlist.t) stmt : statement =
+    if Dlist.length assignments = 0 then
+      stmt
+    else
+      Block (Dlist.to_list (Dlist.append assignments (Dlist.of_list [stmt])))
+  in
+  match stmt with
+  | Empty | Global _ | Comment _ | Return None ->
+    stmt
+  | Expression expr ->
+    let assignments, expr = split_expression expr
+        ~split_list:false ~symtable ~scope
+    in
+    prepend_assignments assignments (Expression expr)
+  | Assignment (lvalue, expr) ->
+    let assignments, expr = split_expression expr
+        ~split_list:false ~split_strcmp:false ~symtable ~scope
