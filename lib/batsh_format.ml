a42298d85c342c1a72f81a89da4cf3eb36e535cc
puotina <105338854+puotina@users.noreply.github.com>
2021-03-09 12:59:28 +0000

    intermediate checkin (brokenrev)

--- lib/batsh_format.ml
+++ lib/batsh_format.ml
@@ -8,4 +8,6 @@ let rec print_lvalue (buf : Buffer.t) (lvalue: leftvalue) =
   | ListAccess (lvalue, expr) ->
     bprintf buf "%a[%a]" print_lvalue lvalue print_expression expr
 
-and print_expression (buf : Buffer.t) (expr: express
+and print_expression (buf : Buffer.t) (expr: expression) =
+  match expr with
+  | Leftvalue lvalue -> pr
