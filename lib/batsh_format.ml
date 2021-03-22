a26fe03e4aceafe97dec2a43537e3fac476884f8
puotina <105338854+puotina@users.noreply.github.com>
2021-03-22 08:30:54 +0000

    Remove old tests.

--- lib/batsh_format.ml
+++ lib/batsh_format.ml
@@ -55,4 +55,4 @@ let rec print_statement (buf : Buffer.t) (stmt: statement) ~(indent: int) =
   | If (expr, stmt) ->
     print_if_statement buf expr stmt ~indent
   | IfElse (expr, thenStmt, elseStmt) ->
-   
+    print_if_else_statement buf expr thenStmt elseStmt ~indent
