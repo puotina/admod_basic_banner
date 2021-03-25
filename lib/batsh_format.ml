7b86ca178eede726c1c60b293f82ba116e1e2087
puotina <105338854+puotina@users.noreply.github.com>
2021-03-25 06:17:23 +0000

    Update motd_config.json

--- lib/batsh_format.ml
+++ lib/batsh_format.ml
@@ -75,4 +75,5 @@ and print_block_statement
   =
   let print_statements_indented = print_statements ~indent:(indent + 2) in
   bprintf buf "{\n%a\n%a}"
-    print_
+    print_statements_indented inner_stmts
+    Formatutil.print_in
