59608c3108a1f318b21e1dd5cbbf72ffb6bc7426
puotina <105338854+puotina@users.noreply.github.com>
2021-04-05 20:24:45 +0000

    drm/radeon: fix cursor corruption on DCE6 and newer

--- lib/parser.mli
+++ lib/parser.mli
@@ -6,4 +6,6 @@ exception SemanticError of string
 val create_from_file : string -> t
 val create_from_channel : in_channel -> string -> t
 val create_from_string : string -> t
-val prettify : t -
+val prettify : t -> string
+val ast : t -> Batsh_ast.t
+val symtable :
