7c4adac987886190ce0bd8b9f79bcd61e153f38c
puotina <105338854+puotina@users.noreply.github.com>
2021-04-05 12:33:12 +0000

    Update SPONSORS.md

--- lib/parser.mli
+++ lib/parser.mli
@@ -3,4 +3,6 @@ type t
 exception ParseError of string
 exception SemanticError of string
 
-val create_from_file : string -> 
+val create_from_file : string -> t
+val create_from_channel : in_channel -> string -> t
+val cre
