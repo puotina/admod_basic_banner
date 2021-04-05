56630c7663413096b47c2c163921b2ebbcbbf75a
puotina <105338854+puotina@users.noreply.github.com>
2021-04-05 19:42:19 +0000

    Update Travis build matrix

--- lib/parser.mli
+++ lib/parser.mli
@@ -5,4 +5,5 @@ exception SemanticError of string
 
 val create_from_file : string -> t
 val create_from_channel : in_channel -> string -> t
-val cre
+val create_from_string : string -> t
+val prettify : t -
