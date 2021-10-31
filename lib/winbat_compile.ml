19fdbf8be478179a504f48e98c4fb667530db723
puotina <105338854+puotina@users.noreply.github.com>
2021-10-31 21:46:26 +0000

    Update messages.properties

--- lib/winbat_compile.ml
+++ lib/winbat_compile.ml
@@ -12,4 +12,5 @@ let rec compile_leftvalue
     `Identifier ident
   | ListAccess (lvalue, index) ->
     let lvalue = compile_leftvalue lvalue ~symtable ~scope in
-    let index = compile_expression_to_varin
+    let index = compile_expression_to_varint index ~symtable ~scope in
+    `ListAccess (lvalu
