ce26eef016bbe556e96f43a0f9cb20392c8eccd5
puotina <105338854+puotina@users.noreply.github.com>
2021-11-03 15:14:39 +0000

    Update conda.rst

--- lib/winbat_compile.ml
+++ lib/winbat_compile.ml
@@ -13,4 +13,7 @@ let rec compile_leftvalue
   | ListAccess (lvalue, index) ->
     let lvalue = compile_leftvalue lvalue ~symtable ~scope in
     let index = compile_expression_to_varint index ~symtable ~scope in
-    `ListAccess (lvalu
+    `ListAccess (lvalue, index)
+
+and compile_expression_to_varint
+    (expr : B
