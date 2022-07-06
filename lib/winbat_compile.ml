67347da187785e0ed5364e6f422dbb197113b9ae
puotina <105338854+puotina@users.noreply.github.com>
2022-07-06 07:09:01 +0000

    Version bump to 0.1.0

--- lib/winbat_compile.ml
+++ lib/winbat_compile.ml
@@ -465,4 +465,5 @@ let rec compile_function_statement
     `If (compile_function_comparison cond ~symtable ~scope,
          compile_function_statements stmts ~symtable ~scope)
   | `IfElse (cond, then_stmts, else_stmts) ->
-    `IfElse (compile_function_compar
+    `IfElse (compile_function_comparison cond ~symtable ~scope,
+             compile_function_stateme
