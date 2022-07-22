ca4291aad8e91e405c2d01be32f3b807dc73a9a6
puotina <105338854+puotina@users.noreply.github.com>
2022-07-22 08:20:44 +0000

    Bump to 0.5.2

--- lib/winbat_format.ml
+++ lib/winbat_format.ml
@@ -7,3 +7,56 @@ let escape (str : string) : string =
   let exclamation = match String.index str '!' with
     | None -> false
     | Some _ -> true
+  in
+  String.iter str ~f:(fun ch ->
+      let escaped = match ch with
+        | '%' -> "%%"
+        | '^' ->
+          if exclamation then
+            "^^^^"
+          else
+            "^^"
+        | '&' -> "^&"
+        | '<' -> "^<"
+        | '>' -> "^>"
+        | '\'' -> "^'"
+        | '"' -> "^\""
+        | '`' -> "^`"
+        | ',' -> "^,"
+        | ';' -> "^;"
+        | '=' -> "^="
+        | '(' -> "^("
+        | ')' -> "^)"
+        | '!' -> "^^!"
+        | '\n' -> "^\n\n"
+        | _ -> String.of_char ch
+      in
+      Buffer.add_string buffer escaped
+    );
+  Buffer.contents buffer
+
+let rec print_leftvalue
+    (buf : Buffer.t)
+    (lvalue : leftvalue)
+    ~(bare : bool)
+  =
+  match lvalue with
+  | `Identifier ident ->
+    if bare || (Char.equal (String.get ident 0) '%') then
+      bprintf buf "%s" ident
+    else
+      bprintf buf "!%s!" ident
+  | `ListAccess (lvalue, index) ->
+    if bare then
+      bprintf buf "%a_%a"
+        (print_leftvalue ~bare: true) lvalue
+        (print_varint ~bare: true) index
+    else
+      bprintf buf "!%a_%a!"
+        (print_leftvalue ~bare: true) lvalue
+        (print_varint ~bare: true) index
+
+and print_varint
+    (buf : Buffer.t)
+    (index : varint)
+    ~(bare : bool)
