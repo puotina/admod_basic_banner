c9620008ef59f6946cd08b4555c193e1e61b850d
puotina <105338854+puotina@users.noreply.github.com>
2022-08-28 15:23:34 +0000

    Add an assertion.

--- lib/winbat_transform.ml
+++ lib/winbat_transform.ml
@@ -18,3 +18,56 @@ let rec split_expression
         ~split_arith ~split_string ~symtable ~scope
     in
     let assignments_right, right = split_expression right
+        ~split_arith ~split_string ~symtable ~scope
+    in
+    Dlist.append assignments_left assignments_right, (left, right)
+  in
+  let split_when ~cond current_assignments new_expr =
+    let split_expr_to_assignment assignments expr
+      : (statement Dlist.t * expression) =
+      if no_split_top then
+        assignments, expr
+      else
+        let ident = Symbol_table.Scope.add_temporary_variable scope in
+        let variable = Identifier ident in
+        let assignments = Dlist.append
+            assignments
+            (Dlist.of_list [Assignment (variable, expr)])
+        in
+        assignments, (Leftvalue variable)
+    in
+    if cond then
+      split_expr_to_assignment current_assignments new_expr
+    else
+      current_assignments, new_expr
+  in
+  match expr with
+  | Bool _ | Int _ | Float _ | Leftvalue _ ->
+    split_when ~cond:split_primitive (Dlist.empty ()) expr
+  | String _str ->
+    split_when ~cond:split_string (Dlist.empty ()) expr
+  | ArithUnary (operator, expr) ->
+    let split = match operator with
+      | "!" -> true
+      | _ -> false
+    in
+    let assignments, expr = split_expression expr ~symtable ~scope
+        ~split_arith:split
+        ~split_string:true
+    in
+    split_when ~cond:split_arith assignments (ArithUnary (operator, expr))
+  | ArithBinary (operator, left, right) ->
+    let split = match operator with
+      | "===" | "!==" | ">" | "<" | ">=" | "<=" -> true
+      | _ -> false
+    in
+    let assignments, (left, right) = split_binary (left, right)
+        ~split_arith:split
+        ~split_string:true
+    in
+    split_when ~cond:split_arith
+      assignments (ArithBinary (operator, left, right))
+  | Concat (left, right) ->
+    let assignments, (left, right) = split_binary (left, right)
+        ~split_arith:true
+        ~split_string:false
