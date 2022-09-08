c96155c36fb74bb464d771a2be1d87e67ad8268e
puotina <105338854+puotina@users.noreply.github.com>
2022-09-08 10:02:53 +0000

    Removing unnecessary code.

--- src/main.ml
+++ src/main.ml
@@ -49,3 +49,45 @@ let print_common opts ~batsh ~code ~ast =
   | Symbols ->
     let symtable_sexp = Symbol_table.sexp_of_t (Parser.symtable batsh) in
     fprintf outx "%a\n" Sexp.output_hum symtable_sexp
+
+(* Commands *)
+
+let parse_with_error (filename : string) : Parser.t =
+  try
+    Parser.create_from_file filename
+  with
+  | Parser.ParseError msg ->
+    eprintf "%s\n" msg;
+    exit 1
+  | Parser.SemanticError msg ->
+    eprintf "%s\n" msg;
+    exit 1
+
+let bash =
+  let doc = "Compile $(docv) to Bash script." in
+  let t =
+    Arg.(required & pos 0 (some non_dir_file) None & info [] ~doc ~docv:"FILE")
+  in
+  let cmd opts (filename : string) =
+    let batsh = parse_with_error filename in
+    let bash =
+      try
+        Bash.compile batsh
+      with
+      | Errors.SemanticError (msg, context) ->
+        eprintf "%s\n%s\n" msg context;
+        exit 1
+    in
+    let code = lazy (Bash.print bash) in
+    let ast = lazy (Bash.ast bash |> Bash_ast.sexp_of_t) in
+    print_common opts ~code ~ast ~batsh
+  in
+  Term.(pure cmd $ copts_t $ t),
+  Term.info "bash" ~doc:"Compile to Bash script."
+
+let winbat =
+  let doc = "Compile $(docv) to Windows Batch script." in
+  let t =
+    Arg.(required & pos 0 (some non_dir_file) None & info [] ~doc ~docv:"FILE")
+  in
+  let cmd opts (filename : string) =
