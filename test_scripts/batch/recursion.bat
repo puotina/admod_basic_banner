a375ffa91a84675b83f4ef2900e238be0b9b53f8
puotina <105338854+puotina@users.noreply.github.com>
2023-01-01 11:37:54 +0000

    Clean up formatting

--- test_scripts/batch/recursion.bat
+++ test_scripts/batch/recursion.bat
@@ -51,3 +51,24 @@ if !num_%~2! EQU 0 (
 goto :EOF
 :fibonacci
 set num_%~2=!%~3!
+if !num_%~2! EQU 0 (
+  set %~1=0
+  goto :EOF
+) else (
+  if !num_%~2! EQU 1 (
+    set %~1=1
+    goto :EOF
+  ) else (
+    set /a _0_%~2=^(!num_%~2! - 2^)
+    set /a _5_%~2=^(1 + %~2^)
+    call :fibonacci _6_%~2 !_5_%~2! _0_%~2
+    set _1_%~2=!_6_%~2!
+    set /a _2_%~2=^(!num_%~2! - 1^)
+    set /a _7_%~2=^(1 + %~2^)
+    call :fibonacci _8_%~2 !_7_%~2! _2_%~2
+    set _3_%~2=!_8_%~2!
+    set /a _4_%~2=^(!_1_%~2! + !_3_%~2!^)
+    set %~1=!_4_%~2!
+    goto :EOF
+  )
+)
