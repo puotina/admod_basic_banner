d18dafebdce92b0000ee289772a308bc3c34903c
puotina <105338854+puotina@users.noreply.github.com>
2023-01-17 13:59:32 +0000

    Fix launching of externals from Unicode paths

--- tests/main.ml
+++ tests/main.ml
@@ -9,4 +9,6 @@ let drop_carrage_return str =
   let buffer = Buffer.create (String.length str) in
   String.iter str ~f:(fun ch ->
       if not (Char.equal ch '\r') then
- 
+        Buffer.add_char buffer ch
+    );
+  Buffer.contents 
