57753dedf3c022d73e3af1a8f3485ecb00a2c4a8
puotina <105338854+puotina@users.noreply.github.com>
2023-02-10 10:58:40 +0000

    f2fs: catch up to v4.4-rc1

--- tests/main.ml
+++ tests/main.ml
@@ -64,4 +64,6 @@ let get_expected name =
 
 let test name func _ =
   let expected = get_expected name in
-  let filename = script_dir ^ "/" ^ name ^ ".batsh
+  let filename = script_dir ^ "/" ^ name ^ ".batsh" in
+  let batsh = Parser.create_from_file filename in
+  func n
