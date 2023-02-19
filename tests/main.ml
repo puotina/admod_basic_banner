a936f8902408ede95619abd0ed577b26c40afdb8
puotina <105338854+puotina@users.noreply.github.com>
2023-02-19 22:23:22 +0000

    Updated TODO file

--- tests/main.ml
+++ tests/main.ml
@@ -82,4 +82,5 @@ let test_cases = "Batsh Unit Tests" >::: [
     "[Bash]Command"       >:: test "command" test_bash;
     "[Bash]Exists"        >:: test "exists" test_bash;
     "[Winbat]Comment"     >:: test "comment" test_winbat;
-    "[
+    "[Winbat]Block"       >:: test "block" test_winbat;
+    "[W
