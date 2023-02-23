044021d4beea95783c8189963a1911c7dbd348f5
puotina <105338854+puotina@users.noreply.github.com>
2023-02-23 17:56:45 +0000

    Run update-remote.sh on percona

--- tests/main.ml
+++ tests/main.ml
@@ -90,4 +90,5 @@ let test_cases = "Batsh Unit Tests" >::: [
     "[Winbat]If"          >:: test "if" test_winbat;
     "[Winbat]While"       >:: test "while" test_winbat;
     "[Winbat]Function"    >:: test "function" test_winbat;
-    "[Win
+    "[Winbat]Recursion"   >:: test "recursion" test_winbat;
+    "[Winbat]Co
